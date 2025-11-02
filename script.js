8aa696670b5c0723a34d6e35a7e9a1855452642a
Prisha <p.garg1606@gmail.com>
2025-11-02 11:53:51 -0500

    added DOM elements for the features

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const displayTime = document.getElementById("time-display")
+const target = document.getElementById('target');
+const typedInput = document.getElementById('typed');
+const wpm = document.getElementById('wpm');
+const accuracy = document.getElementById('accuracy');
+const keystrokes = document.getElementById('keystrokes');
+const bestWpm = document.getElementById('best-wpm');
+const finalWpm = document.getElementById('final-wpm');
+const finalAcc = document.getElementById('final-acc');
+const newTextBtn = document.getElementById('new-text');
+const startBtn = document.getElementById('start-btn');
+const resetBtn = document.getElementById('reset-btn');
+const finishBtn = document.getElementById('finish');
+const mode = document.getElementById('mode');
